6ae22c54d2610d9129366d8be23a6b228e36ad8f
Aravind_Datla <Aravind_Datla@cat.com>
2016-06-12 13:21:43 -0500

    Added List View and Response

--- app/src/main/java/com/example/aravind/app/Second_page.java
+++ app/src/main/java/com/example/aravind/app/Second_page.java
@@ -1,25 +1,41 @@
 package com.example.aravind.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Second_page extends AppCompatActivity {
 
+    private ListView listView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
-        String myStringArray[] = {"First_Item", "Second Item"};
+        String[] myStringArray = new String[]{"First Item", "Second Item", "Third Item"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, myStringArray);
-        ListView listView = (ListView) findViewById(R.id.mylist);
+        listView = (ListView) findViewById(R.id.mylist);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(mMessageClickedHandler);
+
     }
 
+
+    private AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            if( position == 1) {
+                Intent k = new Intent(getApplicationContext(), First_page.class);
+                startActivity(k);
+            }
+        }
+    };
 }
